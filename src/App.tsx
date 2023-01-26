a22c59863ae79ddff3d430fdd8ddb6b2fc2547d0
Dmitry Manannikov <email@slonoed.net>
2023-01-25 21:53:18 -0800

    Create a simple form for ingridients

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
+import { useState, useEffect, SyntheticEvent } from 'react'
+import { DataStore } from 'aws-amplify'
 import logo from './logo.svg'
 import './App.css'
+import { Ingredient } from './models'
+;(async function () {
+  const ingridients = await DataStore.query(Ingredient)
+  console.log(ingridients)
+})()
 
 function App() {
+  const [name, setName] = useState('')
+  const [all, setAll] = useState<Ingredient[]>([])
+
+  useEffect(() => {
+    DataStore.query(Ingredient).then(setAll)
+  }, [])
+
+  const handleSubmit = async (e: SyntheticEvent) => {
+    e.preventDefault()
+    const i = await DataStore.save(new Ingredient({ name }))
+    setName('')
+    setAll(a => [...a, i])
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {all.map(i => (
+          <li key={i.id}>{i.name}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        <button type="submit">Create</button>
+      </form>
     </div>
   )
 }
